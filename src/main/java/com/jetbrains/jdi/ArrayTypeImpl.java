c6877c47491e7fb43b538fab97cde8b28c525720
Egor Ushakov <egor.ushakov@jetbrains.com>
2020-09-08 11:56:46 +0300

    8241080: Consolidate signature parsing code in serviceability tools
    Reviewed-by: sspitsyn, cjplummer

--- src/main/java/com/jetbrains/jdi/ArrayTypeImpl.java
+++ src/main/java/com/jetbrains/jdi/ArrayTypeImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -70,7 +70,8 @@ public class ArrayTypeImpl extends ReferenceTypeImpl
     }
 
     public String componentSignature() {
-        return signature().substring(1); // Just skip the leading '['
+        JNITypeParser sig = new JNITypeParser(signature());
+        return sig.componentSignature();
     }
 
     public String componentTypeName() {
