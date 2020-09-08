b70a862fe42754e58853e124eaac764d2e3c5910
Egor Ushakov <egor.ushakov@jetbrains.com>
2020-09-08 11:51:12 +0300

    8238358: Implementation of JEP 371: Hidden Classes

--- src/main/java/com/jetbrains/jdi/JNITypeParser.java
+++ src/main/java/com/jetbrains/jdi/JNITypeParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -56,49 +56,46 @@ public class JNITypeParser {
         this.signature = signature;
     }
 
-    public static String typeNameToSignature(String signature) {
+    public static String typeNameToSignature(String typeName) {
         StringBuilder sb = new StringBuilder();
-        int firstIndex = signature.indexOf('[');
+        int firstIndex = typeName.indexOf('[');
         int index = firstIndex;
         while (index != -1) {
             sb.append('[');
-            index = signature.indexOf('[', index + 1);
+            index = typeName.indexOf('[', index + 1);
         }
 
         if (firstIndex != -1) {
-            signature = signature.substring(0, firstIndex);
+            typeName = typeName.substring(0, firstIndex);
         }
 
-        switch (signature) {
-            case "boolean":
-                sb.append('Z');
-                break;
-            case "byte":
-                sb.append('B');
-                break;
-            case "char":
-                sb.append('C');
-                break;
-            case "short":
-                sb.append('S');
-                break;
-            case "int":
-                sb.append('I');
-                break;
-            case "long":
-                sb.append('J');
-                break;
-            case "float":
-                sb.append('F');
-                break;
-            case "double":
-                sb.append('D');
-                break;
-            default:
-                sb.append('L');
-                sb.append(signature.replace('.', '/'));
-                sb.append(';');
-                break;
+        if (typeName.equals("boolean")) {
+            sb.append('Z');
+        } else if (typeName.equals("byte")) {
+            sb.append('B');
+        } else if (typeName.equals("char")) {
+            sb.append('C');
+        } else if (typeName.equals("short")) {
+            sb.append('S');
+        } else if (typeName.equals("int")) {
+            sb.append('I');
+        } else if (typeName.equals("long")) {
+            sb.append('J');
+        } else if (typeName.equals("float")) {
+            sb.append('F');
+        } else if (typeName.equals("double")) {
+            sb.append('D');
+        } else {
+            sb.append('L');
+            index = typeName.indexOf("/");   // check if it's a hidden class
+            if (index < 0) {
+                sb.append(typeName.replace('.', '/'));
+            } else {
+                sb.append(typeName.substring(0, index).replace('.', '/'));
+                sb.append(".");
+                sb.append(typeName.substring(index + 1));
+            }
+            sb.append(';');
         }
 
         return sb.toString();
@@ -226,7 +223,14 @@ public class JNITypeParser {
                                                  currentIndex);
                 String retVal = signature.substring(currentIndex,
                                                     endClass);
-                retVal = retVal.replace('/','.');
+                int index = retVal.indexOf(".");
+                if (index < 0) {
+                    retVal = retVal.replace('/', '.');
+                } else {
+                    // hidden class
+                    retVal = retVal.substring(0, index).replace('/', '.')
+                            + "/" + retVal.substring(index + 1);
+                }
                 currentIndex = endClass + 1;
                 return retVal;
 
