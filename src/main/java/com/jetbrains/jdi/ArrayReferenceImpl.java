ed1840da2daebc5741818296e0e52da2fad126af
Fairoz Matte <fmatte@openjdk.org>
2021-05-21 11:17:26 +0300

    8221503: vmTestbase/nsk/jdb/eval/eval001/eval001.java fails with: com.sun.jdi.InvalidTypeException: Can't assign double[][][] to double[][][]

--- src/main/java/com/jetbrains/jdi/ArrayReferenceImpl.java
+++ src/main/java/com/jetbrains/jdi/ArrayReferenceImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -300,7 +300,7 @@ public class ArrayReferenceImpl extends ObjectReferenceImpl
                 String srcComponentSignature =
                     srcParser.componentSignature(destDims);
                 Type srcComponentType =
-                    arrayType().findComponentType(srcComponentSignature);
+                    arrayType().findType(srcComponentSignature);
                 valid = ArrayTypeImpl.isComponentAssignable(destComponentType,
                                                           srcComponentType);
             }
@@ -339,7 +339,7 @@ public class ArrayReferenceImpl extends ObjectReferenceImpl
             return arrayType().componentSignature();
         }
         public Type findType(String signature) throws ClassNotLoadedException {
-            return arrayType().findComponentType(signature);
+            return arrayType().findType(signature);
         }
 
         @Override
