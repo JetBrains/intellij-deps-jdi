3e8aae91687b5767ef2513c7d0faca833ebfecc7
Chris Plummer <cjplummer@openjdk.org>
2023-04-26 12:01:43 +0200

    8300810: Get rid of unused JDI removeListener() methods
    Reviewed-by: kevinw, amenkov

--- src/main/java/com/jetbrains/jdi/VMState.java
+++ src/main/java/com/jetbrains/jdi/VMState.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -223,17 +223,6 @@ class VMState {
 
     synchronized boolean hasListener(VMListener listener) {
         return listeners.stream().anyMatch(ref -> listener.equals(ref.get()));
-    }
-
-    synchronized void removeListener(VMListener listener) {
-        Iterator<WeakReference<VMListener>> iter = listeners.iterator();
-        while (iter.hasNext()) {
-            WeakReference<VMListener> ref = iter.next();
-            if (listener.equals(ref.get())) {
-                iter.remove();
-                break;
-            }
-        }
     }
 
     List<ThreadReference> allThreads() {
